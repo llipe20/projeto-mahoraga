c33d12c68115802dec1e2dd65f65b14bd32847f2
Felipe <silvahlipe@gmail.com>
2023-12-30 23:34:03 -0300

    Criação componente de msg

---
+++ src/components/Messeger.tsx
@@ -0,0 +1,13 @@
+import classNames from 'classnames'
+import React from 'react'
+
+const Messeger: React.FC = () => {
+
+    const MessegeClass: classNames = {}
+
+    return (
+        <div className="flex justify-center items-center gap-3 w-auto h-10 pl-3 pr-3">
+
+        </div>
+    )
+}
